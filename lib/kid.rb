1a6c205ddd4280772905e4fb49bf48a4927d34c6
Tabassum Hassan <tiram819@gmail.com>
2020-04-02 03:47:30 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid 
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
+  class Kid
+  extend MetaDancing
+end
+end
